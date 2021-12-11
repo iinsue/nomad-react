a5a8a89d631a03073c87e837ba5fabf7afb44783
iinsue <hinsu93@naver.com>
2021-12-11 19:39:24 +0900

    #Chapter6 Coin Tracker Challenge
    input money

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import { useState, useEffect } from "react";
 const App = () => {
   const [loading, setLoading] = useState(true);
   const [coins, setCoins] = useState([]);
+  const [money, setMoney] = useState(0);
+  const [chooseCoin, setChooseCoin] = useState();
+  const [convert, setConvert] = useState(0);
+  const [result, setResult] = useState(true);
+
   useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers")
       .then((response) => response.json())
@@ -11,20 +16,55 @@ const App = () => {
         setLoading(false);
       });
   }, []);
+
+  const onListChange = (event) => {
+    setChooseCoin(event.target.value);
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    const list = chooseCoin.split(":");
+    const coinPrice = parseFloat(list[1]);
+    if (money === 0) {
+      return;
+    } else {
+      setConvert(money / coinPrice);
+      setResult(false);
+    }
+  };
+
+  const onMoneyChange = (event) => {
+    setMoney(event.target.value);
+    console.log(event.target.value);
+  };
+
   return (
     <div>
       <h1>The Coins {loading ? null : `(${coins.length})`}</h1>
       {loading ? (
         <strong>Loading...</strong>
       ) : (
-        <select>
+        <select onChange={onListChange}>
+          <option>Choose your Coin</option>
           {coins.map((coin) => (
             <option key={coin.id}>
-              {coin.name} ({coin.symbol}): {coin.quotes.USD.price}USD
+              {coin.name} ({coin.symbol}):{coin.quotes.USD.price}USD
             </option>
           ))}
         </select>
       )}
+      <form onSubmit={onSubmit}>
+        <label htmlFor="USD">USD : </label>
+        <input
+          id="USD"
+          type="text"
+          value={money}
+          onChange={onMoneyChange}
+          placeholder="Money"
+        />
+        <button>Convert</button>
+      </form>
+      <p>{result ? null : `You can buy ${convert} coin`}</p>
     </div>
   );
 };
