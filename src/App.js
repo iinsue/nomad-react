f4e745c5049347134f978f79032fb83941455f99
iinsue <hinsu93@naver.com>
2021-12-11 22:36:33 +0900

    #Chapter7 Movie App part1
    line 6 ~ 14

--- src/App.js
+++ src/App.js
@@ -2,69 +2,40 @@ import { useState, useEffect } from "react";
 
 const App = () => {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [money, setMoney] = useState(0);
-  const [chooseCoin, setChooseCoin] = useState();
-  const [convert, setConvert] = useState(0);
-  const [result, setResult] = useState(true);
-
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+      )
+    ).json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
   useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
+    getMovies();
   }, []);
-
-  const onListChange = (event) => {
-    setChooseCoin(event.target.value);
-  };
-
-  const onSubmit = (event) => {
-    event.preventDefault();
-    const list = chooseCoin.split(":");
-    const coinPrice = parseFloat(list[1]);
-    if (money === 0) {
-      return;
-    } else {
-      setConvert(money / coinPrice);
-      setResult(false);
-    }
-  };
-
-  const onMoneyChange = (event) => {
-    setMoney(event.target.value);
-    console.log(event.target.value);
-  };
 
   return (
     <div>
-      <h1>The Coins {loading ? null : `(${coins.length})`}</h1>
       {loading ? (
-        <strong>Loading...</strong>
+        <h1>Loading...</h1>
       ) : (
-        <select onChange={onListChange}>
-          <option>Choose your Coin</option>
-          {coins.map((coin) => (
-            <option key={coin.id}>
-              {coin.name} ({coin.symbol}):{coin.quotes.USD.price}USD
-            </option>
+        <div>
+          {movies.map((movie) => (
+            <div key={movie.id}>
+              <img src={movie.medium_cover_image} alt="movie" />
+              <h2>{movie.title}</h2>
+              <p>{movie.summary}</p>
+              <ul>
+                {movie.genres.map((g) => (
+                  <li key={g}>{g}</li>
+                ))}
+              </ul>
+            </div>
           ))}
-        </select>
+        </div>
       )}
-      <form onSubmit={onSubmit}>
-        <label htmlFor="USD">USD : </label>
-        <input
-          id="USD"
-          type="text"
-          value={money}
-          onChange={onMoneyChange}
-          placeholder="Money"
-        />
-        <button>Convert</button>
-      </form>
-      <p>{result ? null : `You can buy ${convert} coin`}</p>
     </div>
   );
 };
