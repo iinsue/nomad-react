6eae7cb6bbf7c3f7a665d2d2973e0c3262d62717
iinsue <hinsu93@naver.com>
2021-12-12 14:31:00 +0900

    path error correction2

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import Detail from "./routes/Detail";
 
 const App = () => {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Switch>
         <Route path="/movie/:id">
           <Detail />
         </Route>
-        <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />}>
+        <Route path="/" element={<Home />}>
           <Home />
         </Route>
       </Switch>
