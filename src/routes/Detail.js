8cff7744e4a4c8c242b8d09af0c519db0da3db84
iinsue <hinsu93@naver.com>
2021-12-12 11:21:32 +0900

    #Chapter10 Publishing
    Publishing to github.io

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,17 +1,18 @@
 import { useEffect } from "react";
+import { useCallback } from "react";
 import { useParams } from "react-router-dom";
 
 function Detail() {
   const { id } = useParams();
-  const getMovie = async () => {
+  const getMovie = useCallback(async () => {
     const json = await (
       await fetch(`https://yts.mx/api/v2/list_movies.json?movie_id=${id}`)
     ).json();
     console.log(json);
-  };
+  }, [id]);
   useEffect(() => {
     getMovie();
-  }, []);
+  }, [getMovie]);
   return <h1>Detail</h1>;
 }
 
