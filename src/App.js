8a9f837256af7627391ff21e1ae1ca13dd764198
iinsue <hinsu93@naver.com>
2021-12-12 14:06:29 +0900

    path error correction

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ const App = () => {
         <Route path="/movie/:id">
           <Detail />
         </Route>
-        <Route path="/">
+        <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />}>
           <Home />
         </Route>
       </Switch>
